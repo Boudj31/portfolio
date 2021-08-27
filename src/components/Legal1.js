213b6aed8902b5ce33e1188e62d38362bfa06809
Rachid Boudjenane <boudj@Rachids-MacBook-Pro.local>
2021-08-27 15:25:20 +0200

    v1 done

---
+++ src/components/Legal1.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Legal1() {
+
+    return (
+        <>
+            <div>
+                <h3> 1.4 – Les mentions concernant les données personnelles :</h3>
+                Pour que vos mentions légales soient réellement conformes au RGPD (règlement général sur la protection des données), voici les mentions qui doivent apparaître :
+
+                Coordonnées du DPO (Délégué à la protection des données)
+                Finalité du recueil des données
+                Destinataire des données personnelles
+                Droit d’opposition, d’accès et de rectification
+                Mention du droit de déposer une réclamation auprès de la CNIL
+                Idéalement, les informations détaillées consacrées aux Données Personnelles doivent être affichées sur la page de la Politique de Confidentialité. Votre page de mentions légales devrait faire un lien vers cette page.
+            </div>
+
+        </>
+    )
+}
